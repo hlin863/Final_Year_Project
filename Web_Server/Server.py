efd5d447d86f4a7afd77c24d99c034175bfea7d4
hlin863 <zcabhli@ucl.ac.uk>
2022-10-05 13:48:10 +0100

    Tidied up the folders

---
+++ Web_Server/Server.py
@@ -0,0 +1,32 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        select_file_html = "<p>Click on the \"Choose File\" button to upload a file:</p><form action=\"/action_page.php\"><input type=\"file\" id=\"myFile\" name=\"filename\"><input type=\"submit\"></form>"
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<h1>Machine Learning Continuous integration tool</h1>", "utf-8")) # write a title for the page
+        self.wfile.write(bytes(select_file_html, "utf-8")) # provide an option for uploading files
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
