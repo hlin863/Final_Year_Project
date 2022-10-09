2fb1ae6549333418df2bd3d805807f73e7ab0555
hlin863 <zcabhli@ucl.ac.uk>
2022-10-09 16:48:28 +0100

    Updated pre_process_data function
    I updated pre_process_data to clean up the data and the labels. The clean-up converted the labels into numerical values.

--- Prototype/pipeline/train.py
+++ Prototype/pipeline/train.py
@@ -8,6 +8,8 @@ import pandas as pd # import the library to download the dataset
 from sklearn.ensemble import RandomForestClassifier # import the random forest classifier
 from sklearn.model_selection import train_test_split # import the library to train the model
 import joblib # import the library to persist the model
+import pickle # import the library to display the bucket contents
+import boto3 as aws # import the library to upload the model to the bucket
 
 DATA_URL = ('http://bodywork-ml-pipeline-project.s3.eu-west-2.amazonaws.com/data/iris_classification_data.csv')
 
@@ -20,9 +22,23 @@ def main() -> None:
     data = download_dataset(DATA_URL)
     features, labels = pre_process_data(data)
     train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.2) # split the feature and the labels into training and testing sets
+    
+    print(train_features[0 : 5])
+
+    print(train_labels[0 : 5])
+    
     trained_model = train_model(train_features, train_labels)
+
+    print("Accuracy score: {}".format(trained_model.score(test_features, test_labels))) # print the accuracy of the model
+
     persist_model(trained_model)
 
+    # write a function to test if trained items are saved to the bucket
+    # reference: https://stackoverflow.com/questions/24906126/how-to-unpack-pkl-file
+    
+    """
+    test_persist_model()
+    """
 
 # other functions definitions used in main()
 # ...
@@ -35,9 +51,26 @@ def download_dataset(url: str) -> pd.DataFrame:
 def pre_process_data(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     """Pre-process the data."""
     print('Pre-processing data...')
-    features = data.drop('species', axis=1)
-    print(features.head(5)) # test displays the first 5 rows of the features
-    labels = data['species']
+
+    features_columns = [
+        'sepal length (cm)',
+        'sepal width (cm)',
+        'petal length (cm)',
+        'petal width (cm)'
+    ] # define the features columns
+
+    features = data[features_columns].values # create the features dataframe
+
+    # features = data.drop('species', axis=1)
+
+    species_to_digits_map = {
+        'setosa': 0,
+        'versicolor': 1,
+        'virginica': 2
+    }
+
+    labels = data['species'].map(species_to_digits_map).values # create the labels dataframe
+
     return features, labels
 
 
@@ -52,11 +85,27 @@ def train_model(features: pd.DataFrame, labels: pd.DataFrame) -> RandomForestCla
 def persist_model(model: RandomForestClassifier) -> None:
     """Persist the model to the same AWS S3 bucket that contains the original data."""
     print('Persisting model...')
-    model_name = f'iris-classification-model.pkl'
+    model_filename = 'iris-classification-model.pkl'
     model_path = 'iris-classification-model.pkl'
     # path reference: https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
     joblib.dump(model, model_path)
     print(f'Model persisted to {model_path}.')
+
+    # Upload Model to AWS S3
+    aws_s3_name = 'bodywork-ml-pipeline-project'
+
+    try:
+        aws_s3_client = aws.client('s3')
+        aws_s3_client.upload_file(model_filename, aws_s3_name, f'models/{model_filename}')
+    except Exception as e:
+        print('Error uploading model to AWS S3')
+
+def test_persist_model():
+    with open('iris-classification-model.pkl', 'rb') as f:
+        data = pickle.load(f)
+
+        print(data)
+
 
 if __name__ == '__main__':
     main()
